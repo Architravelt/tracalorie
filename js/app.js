2811dbbe7fa76272bbdd1425a2edcd883fd97043
Clare Amos <clare.amos@gmail.com>
2024-07-22 17:46:56 +0100

    set calorie limit

--- js/app.js
+++ js/app.js
@@ -58,6 +58,12 @@ class CalorieTracker {
     this._render();
   }
 
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit;
+    this._displayCaloriesLimit();
+    this._render();
+  }
+
   //private methods
 
   _displayCaloriesTotal() {
@@ -213,6 +219,10 @@ class App {
     document
       .getElementById('reset')
       .addEventListener('click', this._reset.bind(this));
+
+    document
+      .getElementById('limit-form')
+      .addEventListener('submit', this._setLimit.bind(this));
   }
 
   _newItem(type, e) {
@@ -278,6 +288,23 @@ class App {
     document.getElementById('workout-items').innerHTML = '';
     document.getElementById('filter-meals').value = '';
     document.getElementById('filter-workouts').value = '';
+  }
+
+  _setLimit(e) {
+    e.preventDefault();
+    const limit = document.getElementById('limit');
+
+    if (limit.value === '') {
+      alert('Please provide a limit');
+      return;
+    } else {
+      this._tracker.setLimit(+limit.value);
+      limit.value = '';
+
+      const modalEl = document.getElementById('limit-modal');
+      const modal = bootstrap.Modal.getInstance(modalEl);
+      modal.hide();
+    }
   }
 }
 
